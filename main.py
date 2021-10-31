eede497c36ad43e567ae83d0fd38abc9320908f6
Nils Gotzhein <nils.gotzhein@googlemail.com>
2021-10-31 15:52:09 +0100

    add ocr

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import pyautogui
 import time
+import uuid
+import re
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 MOUSE_MOVE_DELAY = 0.3
 
@@ -15,27 +25,33 @@ def start_to_drognan():
 
 
 def shop_open_weapons_tab():
-    btn_weapons_tab = pyautogui.locateOnScreen('btn_weapons_tab.png')
+    btn_weapons_tab = pyautogui.locateOnScreen('btn_weapons_tab.png', region=(314, 77, 190, 90))
     if btn_weapons_tab is None:
-        btn_weapons_tab = pyautogui.locateOnScreen('btn_weapons_tab2.png')
+        btn_weapons_tab = pyautogui.locateOnScreen('btn_weapons_tab2.png', region=(314, 77, 190, 90))
         if btn_weapons_tab is None:
             exit("weapons tab not found")
     move_and_click(btn_weapons_tab.left + (btn_weapons_tab.width / 2),
-                     btn_weapons_tab.top + (btn_weapons_tab.height / 2))
+                   btn_weapons_tab.top + (btn_weapons_tab.height / 2))
 
 
 def search_items():
-    pyautogui.moveTo(846, 441, duration=MOUSE_MOVE_DELAY)  # move cursor outside of merchant window
-    for imageFile in ['grand_scepter.png', 'rune_scepter.png']:
-        for i in pyautogui.locateAllOnScreen(imageFile):
+    pyautogui.moveTo(784, 25, duration=MOUSE_MOVE_DELAY)  # move cursor outside of merchant window
+    item_found = False
+    for imageFile in ['grand_scepter', 'rune_scepter']:
+        for i in pyautogui.locateAllOnScreen(imageFile + '.png', region=(183, 129, 584, 588)):
             pyautogui.moveTo(i.left + (i.width / 2), i.top + (i.height / 2), duration=MOUSE_MOVE_DELAY)
-            return True
-    return False
+            item_description = detect_text(imageFile)
+            if 'PALADIN SKILL LEVELS' in item_description and 'FASTER CAST RATE' in item_description:
+                item_found = True
+    return item_found
 
 
 def exit_shop_window():
     #  pyautogui.typewrite('escape', interval=0)
-    move_and_click(784, 25)  # open merchant window
+    if pyautogui.position() == (784, 25):
+        pyautogui.click()
+    else:
+        move_and_click(784, 25)  # click red x button
 
 
 def drognan_to_out(first_walk):
@@ -53,27 +69,60 @@ def out_to_drognan():
     move_and_click(1145, 321)  # open merchant window
 
 
-if __name__ == '__main__':
-    pyautogui.PAUSE = 1.0
-    pyautogui.FAILSAFE = True
+def detect_text(item_type):
+    filename = item_type + '_' + str(uuid.uuid4()) + '.png'
+    img = pyautogui.screenshot(filename)
+    text = pytesseract.image_to_string(img, 'eng')
 
-    time.sleep(2)  # Sleep for 3 seconds
+    m = re.search(': [0-9]{4,}(.+UNDEAD)', text, re.DOTALL)
+    if m:
+        text = m.group(1).strip()
 
-    print(pyautogui.position())  # current mouse x and y
-    #  exit()
+    file_object = open('log.txt', 'a')  # Open a file with access mode 'a'
+    file_object.write('\n' + '===' + '\n' + text)  # Append 'hello' at the end of file
+    file_object.close()  # Close the file
+    return text
 
+
+def start_at_startpoint():
     first_walk = True
 
     start_to_drognan()
-    
+
     while True:
         shop_open_weapons_tab()
         if search_items() is True:
             print("item found, stopping")
             break
-        
+
         exit_shop_window()
 
         drognan_to_out(first_walk)
         first_walk = False
         out_to_drognan()
+
+
+def start_at_drognan():
+    while True:
+        exit_shop_window()
+
+        drognan_to_out(False)
+        out_to_drognan()
+
+        shop_open_weapons_tab()
+        if search_items() is True:
+            print("item found, stopping")
+            break
+
+
+if __name__ == '__main__':
+    pyautogui.PAUSE = 1.0
+    pyautogui.FAILSAFE = True
+
+    time.sleep(2)  # Sleep for 2 seconds
+
+    print(pyautogui.position())  # current mouse x and y
+    #  exit()
+
+    start_at_startpoint()
+    #  start_at_drognan()
